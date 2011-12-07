af60257914adb6457ff264bc5573489f3a774d10
gwenn.aubert <gwenn.aubert@f83e1197-98e8-09ef-e11e-71f8804ea9c9>
2011-12-07 21:12:30 +0000

    premiere deposition, le projet est à l'état de la fin du partiel...what's next ?

---
+++ OMG_Zombies/src/zombiegame/Character.java
@@ -0,0 +1,115 @@
+package zombiegame;
+
+/**
+ * Parent Character class
+ * 
+ * @author pylaffon
+ * 
+ */
+public abstract class Character {
+        
+        /** name of the character*/
+        protected String name;
+
+        /** represents the health (once down to 0, this character will be destroyed)*/
+        protected int healthPoints; 
+
+        /**
+         * Constructor of Character class.
+         * 
+         * @param name
+         *                name of the character
+         * @param healthPoints
+         *                initial HP
+         */
+        public Character(String name, int healthPoints) {
+                this.name = name;
+                this.healthPoints = healthPoints;
+        }
+
+        // Accessors
+        public String getName() {
+                return name;
+        }
+
+        public int getHealthPoints() {
+                return healthPoints;
+        }
+
+        /**
+         * wether or not this character is a human
+         * @return
+         */
+        public boolean isHuman(){
+                return false;
+        }
+        
+        /**
+         * wether or not this character is a zombie
+         * @return
+         */
+        public boolean isZombie(){
+                return false;
+        }
+        
+        /**
+         * wether or not this character is a vampire
+         * @return
+         */
+        public boolean isVampire(){
+                return false;
+        }
+        
+        
+        /**
+         * Decrease the number of HP by a certain amount. HP cannot go below 0.
+         * 
+         * @param reduction
+         *                number of HP to reduce
+         */
+        public void reduceHealthPoints(int reduction) {
+                healthPoints = healthPoints - reduction;
+                if (healthPoints < 0) {
+                        healthPoints = 0;
+                }
+        }
+
+        /**
+         * Output a character's saying to the screen
+         * 
+         * @param str
+         *                what the character says
+         */
+        public void say(String str) {
+                System.out.println(name + " says: " + str);
+        }
+
+        /**
+         * Method triggered when the character described by the current object
+         * meets another character, and does something to him (for example,
+         * attack).
+         * 
+         * @param c
+         *                the other character that this character meets
+         */
+        public void encounterCharacter(Character c) {
+                // Default action: do nothing
+                System.out.println(name + " meets " + c.name
+                                + " and does not attack!");
+        }
+
+        
+        public void endOfTurn() {
+                // do nothing
+                System.out.println("this is not suposse to be print, check endOfTurn if Character");
+        }
+
+        /**
+         * attack the character c
+         * @param c
+         */
+        protected void attack(Character c) {
+                 this.say(c.getName()+", I'm gonna kill you!");
+                
+        }
+}
---
+++ OMG_Zombies/src/zombiegame/MadZombie.java
@@ -0,0 +1,28 @@
+package zombiegame;
+
+public class MadZombie extends Zombie{
+
+        public MadZombie(String name, int healthPoints) {
+                super(name, healthPoints);
+        }
+        
+        /**
+         * wether or not this character is a MadZombie
+         * @return
+         */
+        public boolean isMadZombie(){
+                return true;
+        }
+        
+        /**
+         * attack as a madZombie the character c
+         */
+        protected void attack(Character c){
+                int hp=c.getHealthPoints();
+                super.attack(c);
+                if(hp!=c.getHealthPoints()){
+                        c.reduceHealthPoints(20);
+                }
+        }
+
+}
